182072cc8006350803a58fb56de3c66d317936ee
hussaingalax <hussaingalax@gmail.com>
2026-09-21 17:50:51 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+window.MEDITATION_CONFIG = {
+  SUPABASE_URL: "https://sftfivfmeynceokltbyb.supabase.co",
+  SUPABASE_ANON_KEY: "PASTE_YOUR_EXISTING_SUPABASE_ANON_KEY_HERE",
+  RAZORPAY_KEY_ID: "PASTE_RAZORPAY_KEY_ID_HERE",
+  PROGRAM_ID: "PASTE_PROGRAM_UUID_HERE",
+  ADMIN_EMAIL: "arithuyilarivom@gmail.com",
+  DEFAULT_LANGUAGE: "en",
+  CONTACT_NAME: "Mr. Ayyappan",
+  CONTACT_PHONE: "9840851295",
+  DEFAULT_MAP_URL: "https://maps.app.goo.gl/8b64EUECDw9Kgw2H8",
+  DEFAULT_VENUE: "RS Plaza, 1st Floor, West Karikalan Street, Adambakkam, Chennai 600 088"
+};
